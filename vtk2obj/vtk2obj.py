51ab5c915c881834accedd081c8f6e3c64a360e2
mdm <mmorehea@mix.wvu.edu>
2015-06-02 14:56:37 -0400

    made vtk slightly more user friendly

--- vtk2obj/vtk2obj.py
+++ vtk2obj/vtk2obj.py
@@ -1,12 +1,10 @@
 import glob
-from PIL import Image
 import os
 import sys
 import subprocess
-import numpy as np
 
-def main():
-	l = 'VCN_c18_dendrite.vtk'
+def main(path):
+	l = path
 	polygons = []
 	pointsTrigger = 0
 	polygonTrigger = 0
@@ -52,25 +50,92 @@ def main():
 	for each in points:
 		if len(each) == 0:
 			continue
-		f.write('v ' + ' '.join(each)  +'\n')
-	f.write('\n')
-	f.write('g mesh \n')
+		f.write('v ' + ' '.join(each)  +'\r\n')
+	f.write('\r\n')
+	f.write('g mesh \r\n')
 	
 	for each in polygons:
-		f.write('f ' + ' '.join(each) + '\n')
+		f.write('f ' + ' '.join(each) + '\r\n')
 	f.write('g')
 	f.close
-	#ss = 'meshlabserver -i out.obj -o ' + l[:-4] +'.obj -s ' + 'smoothAndReduce.mlx'
-	#subprocess.call(ss, shell= True)	
+	ss = 'meshlabserver -i out.obj -o ' + l[:-4] +'.obj -s ' + 'smoothAndReduce.mlx'
+	subprocess.call(ss, shell= True)	
 	
 	
-
+def batch(path):
+	e = glob.glob(path+'/*.vtk')
+	print e
+	for ii,l in enumerate(e):
+		print 'building OBJ for VTK ' + str(l) + '  ' + str(ii+1)+ ' out of ' + str(len(e)+1)
+		polygons = []
+		pointsTrigger = 0
+		polygonTrigger = 0
+		with open(l) as f:
+			for lines in f:
+				jj = lines.strip()
+				jj = jj.split(' ')
+				#print jj
+				if jj[0] == 'POINTS':
+					pointsTrigger = 1
+					points = []
+					continue
+				if jj[0] == 'POLYGONS':
+					pointsTrigger = 0
+					polygonTrigger = 1
+					
+					
+					continue
+				
+				if pointsTrigger == 1:
+					if len(jj) == 1:
+						continue		
+					#print jj
+					try:
+						numStr = jj
+					except:
+						print jj
+					
+					points.append(numStr)
+					continue
+						
+				if polygonTrigger == 1:
+					
+					try:
+						numStr = [str(int(i) + 1) for i in jj[1:]]
+						polygons.append(numStr)
+					except:
+						continue
+					
+					
+		print len(points[-1])
+		f = open('out.obj', 'w')
+		for each in points:
+			if len(each) == 0:
+				continue
+			f.write('v ' + ' '.join(each)  +'\r\n')
+		f.write('\r\n')
+		f.write('g mesh \r\n')
+		
+		for each in polygons:
+			f.write('f ' + ' '.join(each) + '\r\n')
+		f.write('g')
+		f.close
+		ss = 'meshlabserver -i out.obj -o ' + l[:-4] +'.obj -s ' + 'smoothAndReduce.mlx'
+		subprocess.call(ss, shell= True)
 	
 
 
 if __name__ == "__main__":	
-	#path = sys.argv[1] 
-	main()
+	if len(sys.argv) < 2:
+		print 'VTK2OBJ -- written by Michael Morehead'
+		print 'Usage: python vtk2obj [director || file] '
+	diry = False
+	path = sys.argv[1] 
+	
+	if os.path.isdir(path):
+		batch(path)
+	else:
+		main(path)
 
 
 				
